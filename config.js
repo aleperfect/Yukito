177217d85ac19dbeb27ded0ea89b092fe2c52afd
aleperfect <143966062+aleperfect@users.noreply.github.com>
2024-04-15 10:26:50 -0500

    df

--- config.js
+++ config.js
@@ -11,44 +11,44 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
-  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215517489568', '💫 Mystic - Rey Endymion 💫', false],
-  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
-  ['59177601773', '💫 Mystic - By Alba070503 💫', true],
-  ['393518398856','💫 Mystic - Tester Fabri115💫', true],
-  ['593968585283'],
-  ['5219993404349'],
-  ['5219991402134'],
-  ['5492266466080'],
-  ['5219996125657'],
-  ['5218442114446'],
-  ['59894808483'],
-  ['593980586516'], 
-  ['595975740803'],  
-  ['5492266613038'],
-  ['50497150165'],
-  ['51906662557'],
-  ['573183650526'], 
-  ['5217441298510'], 
-  ['5217294888993'],
-  ['595992611272']
+  ['51992621601', '👑 Aleizn 👑', true],
+  ['51992621601', '💫 Aleizn 💫', true],
+  ['51992621601', '💫 Aleizn 💫', true],
+  ['51992621601', '💫 Aleizn 💫', true],
+  ['51992621601', '💫 Aleizn 💫', true],
+  ['51992621601', '💫 Aleizn 💫', true],
+  ['51992621601', '💫 Aleizn 💫', true],
+  ['51992621601', '💫 Aleizn 💫', false],
+  ['51992621601', '💫 Aleizn 💫', true],
+  ['51992621601', '💫 Aleizn3 💫', true],
+  ['51992621601','💫 Aleizn 💫', true],
+  ['51992621601'],
+  ['51992621601'],
+  ['51992621601'],
+  ['51992621601'],
+  ['51992621601'],
+  ['51992621601'],
+  ['51992621601'],
+  ['51992621601'], 
+  ['51992621601'],  
+  ['51992621601'],
+  ['51992621601'],
+  ['51992621601'],
+  ['51992621601'], 
+  ['51992621601'], 
+  ['51992621601'],
+  ['51992621601']
 ];
 
-global.suittag = ['5219993404349'];
-global.prems = ['51995386439'];
+global.suittag = ['51992621601'];
+global.prems = ['51992621601'];
 
 global.packname = 'Sticker';
-global.author = 'The Mystic - Bot';
-global.wm = 'The Mystic - Bot';
-global.titulowm = 'The Mystic - Bot';
-global.titulowm2 = `The Mystic - Bot`
-global.igfg = 'The Mystic - Bot';
+global.author = 'Aleizn';
+global.wm = 'Aleizn';
+global.titulowm = 'Aleizn';
+global.titulowm2 = `Aleizn`
+global.igfg = 'Aleizn';
 global.wait = '*[ ⏳ ] Cargando...*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
